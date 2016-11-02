8c4413c4d9801ba1d92bdeceaf0ea985dd1ad40e
Highbury1913 <wilensky.jason13@gmail.com>
2016-11-03 00:01:51 +0200

    freq loop

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-for (var number = 0; number <= 1000; number ++){
-	console.log(number);
+var array = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+var max= 1;
+var C=0;
+var freq;
+for (var i = 0; i<array.length; i++) {
+    for (var j=i; j<array.length; j++) {
+    	
+    	if (array[i] == array[j])
+    		C++;
+    	if(max<c)
+        {
+        	max=C;
+        	freq=array[i];
+        }
+    }
 }
